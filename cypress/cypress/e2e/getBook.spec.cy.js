c3b9328a2ab0a89d64e24db738303a1e0fc1f072
Pauline MAITRE <pauline.maitre@talan.com>
2022-10-24 23:41:20 +0200

    try to implement API test

---
+++ cypress/cypress/e2e/getBook.spec.cy.js
@@ -0,0 +1,14 @@
+
+describe('get books', () => {
+
+  it('check API response is not empty', () => {
+    cy.request({
+      method: 'GET',
+      url: 'http://127.0.0.1:5000/books/1'
+    })
+        .should((response) => {
+          expect(response.status).to.eq(200)
+          expect(response.body.title).to.be.eq('Innocent');
+        });
+  })
+})
